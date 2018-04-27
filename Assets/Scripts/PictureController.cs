6914c45a6c003c0ef6fb0450a8eaf55d30b993c8
Holy-Knight <vnycall74@naver.com>
2018-04-28 00:52:51 +0900

    Init

---
+++ Assets/Scripts/PictureController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PictureController : MonoBehaviour
+{
+	public int type = -1;
+
+	void Start ()
+	{
+		
+	}
+		
+	void Update ()
+	{
+		
+	}
+
+	public void Select()
+	{
+		transform.localScale = new Vector3(1, 1, 1);
+	}
+}
